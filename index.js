7fcc39c88a9ff06cab522f9844e897433efa2354
Gregory <gregsoninnovo@gmail.com>
2021-10-12 20:47:06 -0500

    add dateformat

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var mqtt = require("mqtt");
 var client = mqtt.connect("mqtt://localhost");
 const mysql = require("mongodb");
+var moment = require("moment");
 
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -17,13 +18,16 @@ client.on("connect", function () {
 
 client.on("message", function (topic, message) {
   // message is Buffer
-  json1 = JSON.parse(message.toString());
-  console.log(json1);
+  //   json1 = JSON.parse(message.toString());
+  let obj = JSON.parse(message.toString());
+  obj["fechaHora"] = moment().format(obj["fechaHora"]);
+  console.log(obj);
+
   //client.publish('topico2', 'mensaje recibido')
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    var dbo = db.db("CaliDatos");
-    dbo.collection("datosNodo").insertOne(json1, function (err, res) {
+    var dbo = db.db("MioDatos");
+    dbo.collection("datosNodo").insertOne(obj, function (err, res) {
       if (err) throw err;
       console.log("1 document inserted");
       db.close();
